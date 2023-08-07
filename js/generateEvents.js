d6a90a3fe6637da64292f2abbcd4314af21e0997
art12354 <art12354@gmail.com>
2023-08-06 20:13:01 -0500

    updated to use asyncronous functions

--- js/generateEvents.js
+++ js/generateEvents.js
@@ -1,33 +1,32 @@
-// GENERATING EXISTING EVENTS
-function generateEvents() {
-  console.log("generating events")
+async function generateEvents() {
+  console.log("generating events");
   var x = "";
-  fetch("https://api.sethcharleston.com/test1")
-    .then(function(response) {
-      var jsonResponse = response.json()
-      console.log(jsonResponse)
-      return jsonResponse.Items;
-    })
-    .then(function(data) {
-      data.sort(function(a, b) {
-        a = new Date(a.when);
-        b = new Date(b.when);
-        return a < b ? -1 : a > b ? 1 : 0;
-      });
-      data.forEach(function(event) {
-        console.log(event);
-        var when = new Date(event.when);
-        x += `<div class="card">
-                <div class="card-body bg-info">
-                  <p>${event.name}</p>
-                  <p>${when.toDateString()} at ${when.toTimeString()}</p>
-                  <p>${event.where}</p>
-                  <p>${event.tickets}</p>
-                  <button class="btn btn-dark mr-3" onclick="editEvent('${event.event}')">edit</button>
-                  <button class="btn btn-danger" onclick="deleteEvent('${event.event}')">delete</button>
-                </div>
-              </div>`;
-        document.getElementById("eventInfo").innerHTML = x;
-      });
+  try {
+    const response = await fetch("https://api.sethcharleston.com/test1");
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+    const data = jsonResponse.Items;
+    data.sort(function(a, b) {
+      a = new Date(a.when);
+      b = new Date(b.when);
+      return a < b ? -1 : a > b ? 1 : 0;
     });
+    data.forEach(function(event) {
+      console.log(event);
+      var when = new Date(event.when);
+      x += `<div class="card">
+              <div class="card-body bg-info">
+                <p>${event.name}</p>
+                <p>${when.toDateString()} at ${when.toTimeString()}</p>
+                <p>${event.where}</p>
+                <p>${event.tickets}</p>
+                <button class="btn btn-dark mr-3" onclick="editEvent('${event.event}')">edit</button>
+                <button class="btn btn-danger" onclick="deleteEvent('${event.event}')">delete</button>
+              </div>
+            </div>`;
+      document.getElementById("eventInfo").innerHTML = x;
+    });
+  } catch (error) {
+    console.error("Error fetching events:", error);
+  }
 }
