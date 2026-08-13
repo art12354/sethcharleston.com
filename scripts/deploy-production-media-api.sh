c54873edfd00470fd582cdd359932c17898272d9
art12354 <35785846+art12354@users.noreply.github.com>
2026-08-13 18:49:19 +0000

    Fix production media CORS deployment

--- scripts/deploy-production-media-api.sh
+++ scripts/deploy-production-media-api.sh
@@ -44,7 +44,7 @@ put_method "$delete_id" POST COGNITO_USER_POOLS
 put_options() {
   local resource="$1" methods="$2"
   aws apigateway put-method --rest-api-id "$REST_API_ID" --resource-id "$resource" --http-method OPTIONS --authorization-type NONE >/dev/null
-  aws apigateway put-integration --rest-api-id "$REST_API_ID" --resource-id "$resource" --http-method OPTIONS --type MOCK --request-templates 'application/json={"statusCode":200}' >/dev/null
+  aws apigateway put-integration --rest-api-id "$REST_API_ID" --resource-id "$resource" --http-method OPTIONS --type MOCK --request-templates '{"application/json":"{\"statusCode\":200}"}' >/dev/null
   aws apigateway put-method-response --rest-api-id "$REST_API_ID" --resource-id "$resource" --http-method OPTIONS --status-code 200 --response-parameters method.response.header.Access-Control-Allow-Headers=true,method.response.header.Access-Control-Allow-Methods=true,method.response.header.Access-Control-Allow-Origin=true >/dev/null
   aws apigateway put-integration-response --rest-api-id "$REST_API_ID" --resource-id "$resource" --http-method OPTIONS --status-code 200 --response-parameters "method.response.header.Access-Control-Allow-Headers='Content-Type,Authorization',method.response.header.Access-Control-Allow-Methods='${methods},OPTIONS',method.response.header.Access-Control-Allow-Origin='*'" >/dev/null
 }
